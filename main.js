3a95da4f1fb425da0cff5a2a700a312848866cc2
Nacaxi <107555990+nacaxi@users.noreply.github.com>
2023-10-25 12:13:13 -0300

    FUNÇÕES

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-document.querySelector('.tecla_chuva');
+function tocaSomChuva(){
+    document.querySelector('#som_tecla_chuva').play();
+}
 
-alert("olá mundo")
+function tocaSomPassaro(){
+    document.querySelector('#som_tecla_passaro').play();
+}
+
+function tocaSomFloresta(){
+    document.querySelector('#som_tecla_floresta').play();
+}
+
+function tocaSomTrovoes(){
+    document.querySelector('#som_tecla_trovoes').play();
+}
+
+function tocaSomFogueira(){
+    document.querySelector('#som_tecla_fogueira').play();
+}
+
+function tocaSomMar(){
+    document.querySelector('#som_tecla_mar').play();
+}
+
+document.querySelector('.tecla chuva').onclick=tocaSomChuva;
+
+document.querySelector('.tecla passaro').onclick=tocaSomPassaro;
