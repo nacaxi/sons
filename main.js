ac4f4dcb7ccda46c99f46c2fdfd8ba77042d304a
Nacaxi <107555990+nacaxi@users.noreply.github.com>
2023-11-22 12:14:24 -0300

    AULA: 22 DE NOVEMBRO DE 2023

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-function tocaSom(){
-    document.querySelector('#som_tecla_chuva').play();
+
+
+function tocaSom(idElemento){
+    document.querySelector(idElemento).play();
 }
 
 const listadeTeclas = document.querySelectorAll('.tecla')
 
 let contador = 0;
-while (contador < listadeTeclas.length){
-    listadeTeclas[contador].onclick = tocaSomChuva;
-    contador = contador + 1;
-}
 
-function somaValores(a,b){
-    console.log(a + b)
+while (contador < listadeTeclas.length){
+    const instrumento = listadeTeclas[contador].classList
+    console.log(instrumento[1])
+    listadeTeclas[contador].onclick = function (){
+
+        tocaSom(`#som_${instrumento}`);
+    }
+
+    contador = contador + 1;
 }
